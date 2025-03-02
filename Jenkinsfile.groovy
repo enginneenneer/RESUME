4d3e16d53b9df25ec487329f98fbdb129e46c83c
AlexO <132946448+Adinaos@users.noreply.github.com>
2025-03-02 18:21:53 +0200

    Fix jenkins ifle

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,12 @@
 pipeline {
-        agent any
-        stages {
-                stage('Check HTML & JS Quality') {
-                        sh 'npm install -g htmlint eslint'
-                        sh 'htmlint index.html'
-                        sh 'eslint js/script.js --fix' 
-                }
+    agent any
+    stages {
+        stage('Check HTML & JS Quality') {
+            steps {
+                sh 'npm install -g htmlint eslint'
+                sh 'htmlint index.html'
+                sh 'eslint js/script.js --fix'
+            }
         }
+    }
 }
