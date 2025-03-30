30b037dcda6cdcafb939384115c44483685892a7
AlexO <132946448+Adinaos@users.noreply.github.com>
2025-03-30 18:27:23 +0300

    Add eslint check to js

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {
                 bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "npm install -g htmlint eslint"'
                 bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "htmlint index.html"'
-                //bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "eslint js/script.js --fix"'
+                bat '"C:\\Program Files\\Git\\bin\\bash.exe" -c "eslint js/script.js --fix"'
             }
         }
     }
